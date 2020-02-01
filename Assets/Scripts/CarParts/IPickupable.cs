8568bb40ac0ffc2f01726a73db60903426c8a9b1
Jayne Gale <jaynegale@gmail.com>
2020-02-01 19:28:06 +1300

    New prefabs

---
+++ Assets/Scripts/CarParts/IPickupable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IPickupable
+{
+    int ItemID { get; set; }
+    void Unwieldy(int UnwieldyFactor);
+}
