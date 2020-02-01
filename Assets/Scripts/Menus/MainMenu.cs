7de7dbc149a3c464495e1fb945e26e7c684b1c7a
Jayne Gale <jaynegale@gmail.com>
2020-02-01 14:21:26 +1300

    Placeholder main menu UI

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    int _thisSceneBuildIndex;
+
+    private void Start()
+    {
+        _thisSceneBuildIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    public void PlayGame()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(_thisSceneBuildIndex + 1);
+        //WinTrigger.isWin = false;
+    }
+
+    public void ReallyQuit()
+    {
+        Application.Quit();
+    }
+
+}
