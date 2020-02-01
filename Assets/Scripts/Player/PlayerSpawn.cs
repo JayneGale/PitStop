64890f8b980098259dd85fbede5ff844c4da3d9f
Unknown <smoatwork@gmail.com>
2020-02-01 13:38:46 +1300

    Hole obstacle started

---
+++ Assets/Scripts/Player/PlayerSpawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour
+{
+    public Vector2 spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPoint = transform.position;
+    }
+
+    public void Respawn()
+    {
+        transform.position = spawnPoint;
+        GetComponent<Renderer>().enabled = true;
+    }
+}
