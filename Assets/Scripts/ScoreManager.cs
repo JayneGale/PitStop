07c8c982623842d95b913898bdf254d734f5cf04
Moss Cantwell <mossman2142@gmail.com>
2020-02-02 11:16:28 +1300

    merge scoring

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int score;
+    public Text scoreText;
+    void Start()
+    {
+        score =0;
+    }
+    public void scoreUp(){
+        score++;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        scoreText.text= "SCORE: " + score;
+    }
+}
