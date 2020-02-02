bdfbb1926ee2775aa7946d81c993661fee3adefc
Connor <MXCapturing@gmail.com>
2020-02-02 15:38:13 +1300

    Main Menu Navigation

---
+++ Assets/Scripts/Menus/MenuNavigation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class MenuNavigation : MonoBehaviour
+{
+    public Button[] menuButtons;
+
+    void OnEnable()
+    {
+        Invoke("Highlight", 0.1f);
+    }
+
+    void Highlight()
+    {
+        menuButtons[0].Select();
+    }
+}
