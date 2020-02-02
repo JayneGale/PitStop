571a56032173c00f486495800f13b4baffd96d1f
Moss Cantwell <mossman2142@gmail.com>
2020-02-02 16:29:11 +1300

    Moving Car - No reset yet

--- Assets/Scripts/CarParts/CarController.cs
+++ Assets/Scripts/CarParts/CarController.cs
@@ -8,11 +8,12 @@ public class CarController : MonoBehaviour
     public bool[] isPartThere;
     public bool repaired = false;
     // Start is called before the first frame update
+    public Transform startPos;
     public Transform destination;
-        // Movement speed in units per second.
+    // Movement speed in units per second.
 
     public Transform pitstop;
-    public ScoreManager score;
+    public ScoreManagerV2 score;
     public bool haveScored;
     public float speed = 1.0F;
 
@@ -20,17 +21,25 @@ public class CarController : MonoBehaviour
     private float startTime;
 
     // Total distance between the markers.
-    private float journeyLength;
+    private float repairedPath;
+    private float comingtoShop;
+    private float pitstopDist;
+    private float fixedPathDist;
+    public int scoreValue = 20;
+    public float timeout = 30;
+    private float timer;
     void Start()
     {
-        startTime = Time.time;
-        journeyLength = Vector3.Distance(gameObject.transform.position,destination.position);
-        haveScored =false;
+        gameObject.transform.position = startPos.position;
+        repairedPath = Vector3.Distance(gameObject.transform.position, destination.position);
+        comingtoShop = Vector3.Distance(gameObject.transform.position, pitstop.position);
+        haveScored = false;
+        timer = timeout;
     }
 
     // Update is called once per frame
     void FixedUpdate()
-    {   
+    {
         var fixedpartcount = 0;
         for (int i = 0; i < carParts.Length; i++)
         {
@@ -40,23 +49,60 @@ public class CarController : MonoBehaviour
                 fixedpartcount++;
             }
         }
-        if(fixedpartcount == carParts.Length){
-            repaired =true;
-            if(haveScored==false)
-            {   
-                //score.ScoreUp(1);
-                haveScored =true;
+        if (fixedpartcount == carParts.Length)
+        {
+            repaired = true;
+            if (haveScored == false)
+            {
+                score.ScoreUp(scoreValue);
+                haveScored = true;
             }
         }
-        if(repaired){
+        if (repaired)
+        {
             //move the car closer
             //find the direction between gameObject.transform and destination
             //translate by some speed factor
             // Distance moved equals elapsed time multiplied by speed..
-            float distCovered = (Time.time - startTime) * speed* Time.deltaTime;
+            fixedPathDist += speed * Time.deltaTime;
             // Fraction of journey completed equals current distance divided by total distance.
-            float fractionOfJourney = distCovered / journeyLength;
-            gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, destination.position, fractionOfJourney);
+            float fixedJourneyPercent = fixedPathDist / repairedPath;
+            if (fixedJourneyPercent < .90)
+            {
+                gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, destination.position, fixedJourneyPercent);
+            }
+            else
+            {
+                Recycle();
+            }
         }
+        else
+        {
+            timer -= Time.deltaTime;
+            if(timer<0 && haveScored==false){
+                score.ScoreDown(scoreValue);
+                haveScored=true;
+                Recycle();
+            }
+            pitstopDist += speed * Time.deltaTime;
+            // Fraction of journey completed equals current distance divided by total distance.
+            float fixedJourneyPercent = pitstopDist / comingtoShop;
+            if (fixedJourneyPercent < .90)
+            {
+                gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, pitstop.position, fixedJourneyPercent);
+            }
+        }
+    }
+    /**
+    * re start the state of the car to be used again.
+    */
+    private void Recycle()
+    {
+        haveScored = false;
+        repaired = false;
+        timer = timeout;
+        gameObject.transform.position = startPos.position;
+        pitstopDist=0;
+        //do some logic to create broken parts
     }
 }
